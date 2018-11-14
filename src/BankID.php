01dc0f98ba784a8632a6f2ad9a35b291e60cce90
Anders Fajersson <anders@perifer.se>
2018-11-14 20:27:25 +0100

    Simplify constants

--- src/BankID.php
+++ src/BankID.php
@@ -9,18 +9,11 @@ use GuzzleHttp\Exception\RequestException;
 class BankID
 {
     const ENVIRONMENT_PRODUCTION = 'prod';
-    const HOST_PRODUCTION = 'appapi2.bankid.com';
-
     const ENVIRONMENT_TEST = 'test';
-    const HOST_TEST = 'appapi2.test.bankid.com';
 
-    const ENVIRONMENTS = [
-        self::ENVIRONMENT_PRODUCTION => [
-            'host' => self::HOST_PRODUCTION,
-        ],
-        self::ENVIRONMENT_TEST => [
-            'host' => self::HOST_TEST,
-        ],
+    const HOSTS = [
+        self::ENVIRONMENT_PRODUCTION => 'appapi2.bankid.com',
+        self::ENVIRONMENT_TEST => 'appapi2.test.bankid.com',
     ];
 
     private $httpClient;
@@ -35,7 +28,7 @@ class BankID
     public function __construct($certificate, $rootCertificate = null, $environment = self::ENVIRONMENT_TEST)
     {
         $httpOptions = [
-            'base_uri' => 'https://'.self::ENVIRONMENTS[$environment]['host'].'/rp/v5/',
+            'base_uri' => 'https://'.self::HOSTS[$environment].'/rp/v5/',
             'verify' => false,
             'cert' => [
                 $certificate,
