c66b4f2af6cad48d1325ee2629d391d738d59726
Peter Törnstrand <peter@happiness.se>
2020-02-11 17:08:51 +0100

    Add option to use key file and password

--- src/BankID.php
+++ src/BankID.php
@@ -24,11 +24,17 @@ class BankID
      * @param string $environment
      * @param string $certificate
      * @param string $rootCertificate
+     * @param string $key
+     * @param string $passphrase
      */
-    public function __construct($environment = self::ENVIRONMENT_TEST, $certificate = null, $rootCertificate = null)
+    public function __construct($environment = self::ENVIRONMENT_TEST, $certificate = null, $rootCertificate = null, $key = null, $passphrase = null)
     {
         if (! $certificate) {
             $certificate = __DIR__.'/../certs/test.pem';
+        }
+
+        if (! is_null($passphrase)) {
+            $certificate = [$certificate, $passphrase];
         }
 
         if (! $rootCertificate) {
@@ -44,6 +50,12 @@ class BankID
                 'Accept' => 'application/json',
             ],
         ];
+
+        if (! is_null($key)) {
+            $httpOptions['ssl_key'] = $key;
+        }
+
+        
 
         $this->httpClient = new Client($httpOptions);
     }
