2d856cfac3decd905f97997bd780a1fbb0b96c0f
Mohamad Rashid <mohamadrashid@macbook-pro.home>
2022-02-24 19:30:18 +0100

    Removed sign method

--- src/BankID.php
+++ src/BankID.php
@@ -128,41 +128,6 @@ class BankID
     }
 
     /**
-     * Request a signing order for a user.
-     *
-     * @param $personalNumber
-     * @param $ip
-     * @param $userVisibleData
-     * @param $userNonVisibleData
-     *
-     * @return BankIDResponse
-     */
-    public function sign($personalNumber, $ip, $userVisibleData = '', $userNonVisibleData = NULL)
-    {
-        try {
-            $parameters = [
-                'personalNumber' => $personalNumber,
-                'endUserIp' => $ip,
-                'userVisibleData' => base64_encode($userVisibleData),
-            ];
-
-            if (!empty($userNonVisibleData)) {
-                $parameters['userNonVisibleData'] = base64_encode($userNonVisibleData);
-            }
-
-            $httpResponse = $this->httpClient->post('sign', [
-                RequestOptions::JSON => $parameters,
-            ]);
-        } catch (RequestException $e) {
-            return self::requestExceptionToBankIDResponse($e);
-        }
-
-        $httpResponseBody = json_decode($httpResponse->getBody(), true);
-
-        return new BankIDResponse(BankIDResponse::STATUS_PENDING, $httpResponseBody);
-    }
-
-    /**
      * Collect an ongoing user request.
      *
      * @param $orderReference
